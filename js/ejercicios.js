76ce89f5237d614c35d567a4e1824a1dd6e1c8d5
David Romero <dvdrmrrdz@gmail.com>
2022-10-20 02:54:50 -0500

    ejercicios JS de colores y posts agregados

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -59,15 +59,70 @@ const calificar = () => {
 
    if(valor < 0 || valor > 10){
       document.querySelector('#calificacion').innerHTML = `<br><p>El valor debe ser entre 0 y 10</p>`
-   }else if(valor <= 3) {
+   }else if(valor == 0) {
       document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Deficiente 😣</p>`
    }else if(valor <= 5){
       document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Insuficiente 🙁</p>`
+   }else if(valor <= 6){
+      document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Bajo 😐</p>`
    }else if(valor <= 7){
-      document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Regular 😐</p>`
+      document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Regular 🙂</p>`
+   }else if(valor <= 8){
+      document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Suficiente 😃</p>`
    }else if(valor <= 9){
-      document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Excelente 😃</p>`
+      document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Excelente 😁</p>`
    }else if(valor == 10){
       document.querySelector('#calificacion').innerHTML = `<br><p>Desempeño Sobresaliente 😎</p>`
+   }
+}
+
+
+
+/*** COLORES ***/
+const red = () => {
+   let   lienzo = document.querySelector('#lienzo')
+   lienzo.style.backgroundColor = "red"
+}
+const green = () => {
+   let   lienzo = document.querySelector('#lienzo')
+   lienzo.style.backgroundColor = "green"
+}
+const blue = () => {
+   let   lienzo = document.querySelector('#lienzo')
+   lienzo.style.backgroundColor = "blue"
+}
+const yellow = () => {
+   let   lienzo = document.querySelector('#lienzo')
+   lienzo.style.backgroundColor = "yellow"
+}
+const purple = () => {
+   let   lienzo = document.querySelector('#lienzo')
+   lienzo.style.backgroundColor = "purple"
+}
+
+
+
+/*** FAKE TWITTER ***/
+const post = () => {
+   let   msj = document.querySelector('#msj').value,
+         warning = document.querySelector('.warning')
+   
+   if(msj != ""){
+      let   newCard = document.querySelector('.js__twitter'),
+            newField = document.createElement('fieldset'),
+            newLegend = document.createElement('legend'),
+            newMsj = document.createElement('p')
+            
+            newField.appendChild(newLegend)
+            newField.classList.add('view')
+            newLegend.textContent = 'Usuario escribió'
+            newField.appendChild(newMsj)
+            newMsj.textContent = msj
+            newCard.insertAdjacentElement("afterend", newField)
+
+            document.querySelector('#msj').value = ""
+            warning.textContent = ""
+   } else{
+      warning.innerHTML = `<br><p>No puedes postear mensajes vacíos</p>`
    }
 }
