80a3678d924ac2279fc0bcf6435c7c78a85ca45f
David Romero <dvdrmrrdz@gmail.com>
2023-09-14 13:47:45 -0600

    actualizacion archivos json y bio.js

--- src/components/Bio/bio.js
+++ src/components/Bio/bio.js
@@ -2,27 +2,27 @@ export const contacto = [
   {
     title: "Correo Electrónico",
     link: "mailto:dvdrmrrdz@gmail.com",
-    icon: "https://dr-frontend.github.io/portfolio/assets/svgs/contact_instagram.svg",
+    icon: "https://raw.githubusercontent.com/DR-Frontend/portfolio/master/src/assets/svg/contact_mail.svg",
   },
   {
     title: "Cirrículum Vitae",
     link: "#",
-    icon: "https://dr-frontend.github.io/portfolio/assets/svgs/contact_cv.svg",
+    icon: "https://raw.githubusercontent.com/DR-Frontend/portfolio/91dcf8e6ba8eb4720e6f5d4f8bfe9ad601805d27/src/assets/svg/contact_cv.svg",
   },
   {
     title: "LinkedIn",
     link: "https://www.linkedin.com/in/david-romero/",
-    icon: "https://dr-frontend.github.io/portfolio/assets/svgs/contact_linkedin.svg",
+    icon: "https://raw.githubusercontent.com/DR-Frontend/portfolio/91dcf8e6ba8eb4720e6f5d4f8bfe9ad601805d27/src/assets/svg/contact_linkedin.svg",
   },
   {
     title: "Github",
     link: "https://github.com/DR-Frontend",
-    icon: "https://dr-frontend.github.io/portfolio/assets/svgs/contact_github.svg",
+    icon: "https://raw.githubusercontent.com/DR-Frontend/portfolio/91dcf8e6ba8eb4720e6f5d4f8bfe9ad601805d27/src/assets/svg/contact_github.svg",
   },
   {
     title: "Behance",
     link: "https://www.behance.net/david-webdesigner",
-    icon: "https://dr-frontend.github.io/portfolio/assets/svgs/contact_behance.svg",
+    icon: "https://raw.githubusercontent.com/DR-Frontend/portfolio/91dcf8e6ba8eb4720e6f5d4f8bfe9ad601805d27/src/assets/svg/contact_behance.svg",
   }
 ]
 
